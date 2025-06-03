e876cb30be0cb6c8806fbe84aef1be44f816bf90
Randy Oest <oest@amazingrando.com>
2025-06-03 16:53:05 -0400

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,21 @@ export default function Home() {
       <h1 className="text-white text-4xl font-bold">Stanford School of Humanities and Sciences</h1>
       <ul className="flex flex-row gap-4 text-2xl">
         <li>
-          <Link href="/round-3/c" className="border border-white rounded-full px-4 py-2 hover:underline">Round 3C</Link>
-        </li>
-        <li>
-          <Link href="/round-3/d" className="border border-white rounded-full px-4 py-2 hover:underline">Round 3D</Link>
+          <Link href="/round-4/e" className="border border-white rounded-full px-4 py-2 hover:underline">Round 4E</Link>
         </li>
       </ul>
+      
       <div className="text-center">
-        <h2 className="text-white text-2xl font-bold">Round 2</h2>
+        <h2 className="text-white text-2xl font-bold">Round 3</h2>
+        <ul className="flex flex-row justify-center gap-4 text-base">
+          <li>
+            <Link href="/round-3/c" className="hover:underline">Round 3C</Link>
+          </li>
+          <li>
+            <Link href="/round-3/d" className=" hover:underline">Round 3D</Link>
+          </li>
+        </ul>
+        <h2 className="text-white text-2xl font-bold mt-4">Round 2</h2>
         <ul className="flex flex-row justify-center gap-4 text-base">
           <li>
             <Link href="/round-2" className="hover:underline">Round 2</Link>
